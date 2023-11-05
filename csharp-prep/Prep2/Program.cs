5d100be8ad17c7b267f2f66b9600a94b02449727
wstammSeidor <wstamm@seidoranalytics.com>
2023-11-04 22:25:48 -0300

    weeek1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,99 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+
+        string resp = Console.ReadLine();
+
+        int percent = int.Parse(resp);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            if (percent >= 97)
+            {
+                letter = "A+";
+            }
+            else if (percent >= 93)
+            {
+                letter = "A";
+            }
+            else
+            {
+                letter = "A-";
+            }
+        }
+        else if (percent >= 80)
+        {
+            if (percent >= 87)
+            {
+                letter = "B+";
+            }
+            else if (percent >= 83)
+            {
+                letter = "B";
+            }
+            else
+            {
+                letter = "B-";
+            }
+        }
+        else if (percent >= 70)
+        {
+            if (percent >= 77)
+            {
+                letter = "C+";
+            }
+            else if (percent >= 73)
+            {
+                letter = "C";
+            }
+            else
+            {
+                letter = "C-";
+            }
+        }
+        else if (percent >= 60)
+        {
+            if (percent >= 67)
+            {
+                letter = "D+";
+            }
+            else if (percent >= 63)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "D-";
+            }
+        }
+        else
+        {
+            if (percent >= 57)
+            {
+                letter = "F+";
+            }
+            else if (percent >= 53)
+            {
+                letter = "F";
+            }
+            else
+            {
+                letter = "F-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
