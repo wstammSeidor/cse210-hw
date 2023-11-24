9f77dc95f6c1cf8ac752a28cace6395dc5ea056d
wstammSeidor <wstamm@seidoranalytics.com>
2023-11-24 10:19:22 -0300

    week4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assign1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assign1.GetSummary());
+
+        MathAssignment assing2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assing2.GetSummary());
+        Console.WriteLine(assing2.GetHomeworkList());
+
+        WritingAssignment assing3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assing3.GetSummary());
+        Console.WriteLine(assing3.GetWritingInformation());
     }
 }
